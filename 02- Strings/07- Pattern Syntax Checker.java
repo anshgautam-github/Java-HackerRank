90e8cdfe50bd7ac7b3e17abd8cf39449e6747951
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2024-12-17 23:04:54 +0530

    Create 07- Pattern Syntax Checker.java

---
+++ 02- Strings/07- Pattern Syntax Checker.java
@@ -0,0 +1,28 @@
+/*
+Note: In this problem, a regex is only valid if you can compile it using the Pattern.compile method.
+*/
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	//Write your code
+            try{
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }catch(PatternSyntaxException e){
+                System.out.println("Invalid");
+            }
+            testCases--;
+		}
+	}
+}
+
+
+
