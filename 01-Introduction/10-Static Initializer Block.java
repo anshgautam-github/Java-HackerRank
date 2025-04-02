839cf9c170c1f22fa03fea3bb5be195981fc683f
Ansh Gautam <95379812+anshgautam-github@users.noreply.github.com>
2025-04-02 18:48:51 +0530

    Update 10-Static Initializer Block.java

--- 01-Introduction/10-Static Initializer Block.java
+++ 01-Introduction/10-Static Initializer Block.java
@@ -1,42 +1,30 @@
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
+    static int B, H;
+    static boolean flag;
 
-    static int B;  // Breadth of the parallelogram
-    static int H;  // Height of the parallelogram
-    static boolean flag = true;  // Flag to check validity of the inputs
-    
-    // Static block to initialize the values and perform input validation
+    // Static block to initialize variables and validate conditions
     static {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        B = scan.nextInt();
+        H = scan.nextInt();
+        scan.close();
         
-        try {
-            B = sc.nextInt();  // Read breadth
-            H = sc.nextInt();  // Read height
-            
-            // Validate the inputs: both B and H must be positive
-            if (B <= 0 || H <= 0) {
-                flag = false;  // If invalid, set flag to false
-                throw new Exception("Breadth and height must be positive");
-            }
-        } catch (Exception e) {
-            // If an exception occurs (invalid input), print the exception message
+        if (B > 0 && H > 0) {
+            flag = true;
+        } else {
+            flag = false;
             System.out.println("java.lang.Exception: Breadth and height must be positive");
         }
     }
 
-public static void main(String[] args){
-		if(flag){
-			int area=B*H;
-			System.out.print(area);
-		}
-		
-	}//end of main
-
-}//end of class
-
+    public static void main(String[] args) {
+        if (flag) {
+            int area = B * H;
+            System.out.println(area);
+        }
+    }
+}
